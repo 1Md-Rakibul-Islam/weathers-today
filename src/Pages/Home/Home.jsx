98f317cc0c1a4efdf004a6b6f7c8dcd4cfefe172
1Md-Rakibul-Islam <rakibul.islam199tech@gmail.com>
2023-04-01 21:51:27 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FetureBanner from './FetureBanner/FetureBanner';
+
+const Home = () => {
+    return (
+        <div>
+            <FetureBanner />
+        </div>
+    );
+};
+
+export default Home;
